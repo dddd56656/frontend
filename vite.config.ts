3a20b088452031e45f4752ec92406e617cb5d7fb
sww <1248002362@qq.com>
2025-05-14 02:25:42 +0800

    构建测试版本整合

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    checker({
+      vueTsc: true,   // 仍然跑 vue-tsc 类型检查
+      eslint: false   // 屏蔽 eslint 通道，避免报错
+    })
+  ],
 })
